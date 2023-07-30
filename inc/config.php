ddc9a54fcb1bfeea07f52fb9aff8d0640773fbfe
pelandrun <javier.zavalia@gmail.com>
2023-07-30 23:09:08 +0000

    a

--- inc/config.php
+++ inc/config.php
@@ -1,4 +1,8 @@
 <?php
 //aqui van las constantes globales de configuracion
+$Loader = new josegonzalez\Dotenv\Loader(__DIR__ . "/.env");
+$Loader->parse()->define();
+error_log(PERSAT_TOKEN);
+error_log('aaa');
 define("VARIABLE","VALOR");
 define("URI_FILTER","/index.php$/");
